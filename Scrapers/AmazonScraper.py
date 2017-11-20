50bf127798387b9783565c622a9e193922de2a75
thuwarakeshm <thuwarakeshm@stax.com>
2017-11-20 13:04:20 +0530

    amazon working model

--- Scrapers/AmazonScraper.py
+++ Scrapers/AmazonScraper.py
@@ -1,23 +1,106 @@
 import re
-import requests
+# import requests #If using python direct alls
+from selenium import webdriver  # If using selenium webdriver
 import pandas as pd
-
+import time
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 
-def get_review_details(review):
-    return True
-
-
-def get_auther_detail(review):
-    return True
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 
 def get_review(base_url, pages_count, PROXY='127.0.0.9666'):
+    start_time = datetime.now()
+    print(bcolors.OKBLUE + '{} : Initiate Amazon Scraper'.format(datetime.now()))
+
+    # Chrome Options if using a selenium webdriver
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--proxy-server=%s' % PROXY)
+    driver = webdriver.Chrome(r'C:\Users\thuwarakeshm\Documents\scraping\Yelp\chromedriver.exe',
+                              chrome_options=chrome_options)
+
     pages = range(1, pages_count + 1)
-    url_core = re.split(r'pageNumber=', base_url)[0]
+    splitted = re.split(r'paging_btm_', base_url)
 
-    urls = [url_core + 'pageNumber={}'.format(page) for page in pages]
+    urls = [splitted[0] + re.sub(r'\d+', '{}'.format(page), splitted[1]) for page in pages]
 
-    [print(url) for url in urls]
-    return True
+    reviews = []
+
+    for url in urls:
+        print(bcolors.OKBLUE + '{0} : trying to fetch url {1}'.format(datetime.now(), url))
+        try:
+
+            # page = requests.get(url, proxies={'http': PROXY, 'https': PROXY}) #To be used with python direct calls
+            driver.get(url)  # To be used with selenium webdriver
+
+            print(bcolors.OKGREEN + '{0} : Successfully fetched url {1}'.format(datetime.now(), url))
+        except:
+            print(bcolors.FAIL + '{0} : Failed to fetched url {1}'.format(datetime.now(), url))
+            break
+
+        # soup = BeautifulSoup(page.content, 'html.parser') # To be used with python direct calls
+        soup = BeautifulSoup(driver.page_source, 'html.parser')  # To be used with selenium webdriver
+
+        try:
+            reviews_blocks = soup.find('div', id='cm_cr-review_list').find_all('div', attrs={'class': 'review'})
+        except:
+            print(bcolors.FAIL + '{0} : Could not extract reviews for url {1}'.format(datetime.now(), url))
+            break
+
+        for review in reviews_blocks:
+            try:
+                review_num = reviews.index(review) + 1
+            except:
+                review_num = 0
+            try:
+                rating = review.find('a').attrs['title'][0]
+            except:
+                print(bcolors.WARNING + '{0} : Could not extract rating for {2}th review of url {1}'.format(
+                    datetime.now(), url, review_num))
+                rating = 0
+            try:
+                review_title = review.find('a', attrs={'data-hook': 'review-title'}).text
+            except:
+                print(bcolors.WARNING + '{0} : Could not extract review_title for {2}th review of url {1}'.format(
+                    datetime.now(), url, review_num))
+                review_title = 'Unknown'
+            try:
+                author_name = review.find('a', attrs={'data-hook': "review-author"}).text
+            except:
+                print(bcolors.WARNING + '{0} : Could not extract author_name for {2}th review of url {1}'.format(
+                    datetime.now(), url, review_num))
+                author_name = 'Unknown'
+
+            try:
+                review_date = re.search(r'\w+ \d+, \d+',
+                                        review.find('span', attrs={'data-hook': "review-date"}).text).group()
+                # review_date = datetime(review_date)
+            except:
+                print(bcolors.WARNING + '{0} : Could not extract review_date for {2}th review of url {1}'.format(
+                    datetime.now(), url, review_num))
+                review_date = datetime(1990, 1, 1)
+            try:
+                review_text = review.find('span', attrs={'data-hook': "review-body"}).text
+            except:
+                print(bcolors.WARNING + '{0} : Could not extract review_text for {2}th review of url {1}'.format(
+                    datetime.now(), url, review_num))
+                review_text = 'Blank'
+
+            reviews.append(
+                {'rating': rating, 'review_title': review_title,
+                 'author_name': author_name, 'review_date': review_date,
+                 'review_text': review_text})
+            # time.sleep(5)
+
+    driver.close()
+
+    return pd.DataFrame(reviews)
--- examples/amazon_scrapping.py
+++ examples/amazon_scrapping.py
@@ -1,8 +1,12 @@
 from Scrapers.AmazonScraper import get_review
+import time
 
 urls = [
-    '# https://www.amazon.com/Champion-Powerblend-Fleece-Pullover-Hoodie/product-reviews/B01HIURMZU/ref=cm_cr_arp_d_paging_btm_2?ie=UTF8&reviewerType=all_reviews&pageNumber=4']
+    'https://www.amazon.com/Champion-Powerblend-Fleece-Pullover-Hoodie/product-reviews/B01HIURMZU/ref=cm_cr_arp_d_paging_btm_2?ie=UTF8&reviewerType=all_reviews&pageNumber=4']
 
 for url in urls:
-    print(get_review(url, 2))
+    df = get_review(url, 2, PROXY='127.0.0.1:10000')
 
+    df.to_csv(r'C:\Users\thuwarakeshm\Documents\scraping\Yelp\data\dataframe.csv')
+
+    # time.sleep(5)
