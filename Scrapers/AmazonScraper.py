801f146fe90c3f34e8b54cfffc489dcd52c09aca
thuwarakeshm <thuwarakeshm@stax.com>
2017-11-20 10:10:38 +0530

    'basic skelaton'

---
+++ Scrapers/AmazonScraper.py
@@ -0,0 +1,15 @@
+
+import re
+import requests
+import pandas as pd
+
+from bs4 import BeautifulSoup
+
+def get_review_details(review):
+    return True
+
+def get_auther_detail(review):
+    return True
+
+def get_review(review):
+    return True
---
+++ examples/amazon_scrapping.py
@@ -0,0 +1,7 @@
+from Scrapers.AmazonScraper import get_review
+
+urls = [
+    '# https://www.amazon.com/Champion-Powerblend-Fleece-Pullover-Hoodie/product-reviews/B01HIURMZU/ref=cm_cr_arp_d_paging_btm_2?ie=UTF8&reviewerType=all_reviews&pageNumber=4']
+
+for url in urls:
+    print(get_review(url))
